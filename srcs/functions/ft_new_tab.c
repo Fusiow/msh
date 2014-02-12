82301c610e05b9f78b1acebe8f1c1a96509a4282
Louis SOLOFRIZZO <lsolofri@e1r10p23.42.fr>
2014-02-12 17:58:41 +0100

    fix ft_new tab

--- srcs/functions/ft_new_tab.c
+++ srcs/functions/ft_new_tab.c
@@ -6,11 +6,10 @@
 /*   By: aardjoun <aardjoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/27 15:01:34 by aardjoun          #+#    #+#             */
-/*   Updated: 2014/02/11 16:14:05 by aardjoun         ###   ########.fr       */
+/*   Updated: 2014/02/12 17:57:48 by lsolofri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "../../includes/msh.h"
 
 char	**ft_new_tab(int i)
@@ -20,10 +19,7 @@ char	**ft_new_tab(int i)
 
 	n = 0;
 	tab = (char **) malloc((i + 1) * sizeof(char *));
-	while (n < i)
-	{
-		tab[n] = NULL;
-		n++;
-	}
+	while (n <= i)
+		tab[n++] = NULL;
 	return (tab);
 }
