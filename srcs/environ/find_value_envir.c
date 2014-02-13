f9ce5645919e9b0aff3913616a484029375639c0
ne02ptzero <ne02ptzero@dev-box>
2014-02-13 16:11:46 -0600

    Little fixes on find_value

--- srcs/environ/find_value_envir.c
+++ srcs/environ/find_value_envir.c
@@ -5,7 +5,10 @@ char 	*find_value_envir(char *name)
 	int 	i;
 
 	i = 0;
-	while (ft_strncmp(g_env[i], name, ft_strlen(name)))
+	while (ft_strncmp(g_env[i], name, ft_strlen(name)) && g_env[i])
 		++i;
-	return (ft_strsub(g_env[i], ft_strlen(name) + 1, ft_strlen(g_env[i])));
+	if (g_env[i])
+		return (ft_strsub(g_env[i], ft_strlen(name) + 1, ft_strlen(g_env[i])));
+	else
+		return (NULL);
 }
