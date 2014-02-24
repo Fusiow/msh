4e18f9ac33fc068c6665d16cf1886e80b78a6db8
ivory9334 <aardjoun@e2r2p1.42.fr>
2014-02-24 19:45:48 +0100

    mod cd

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -7,7 +7,7 @@
 /*   By: aardjoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/24 16:19:25 by aardjoun          #+#    #+#             */
-/*   Updated: 2014/02/24 18:53:04 by aardjoun         ###   ########.fr       */
+/*   Updated: 2014/02/24 19:44:20 by aardjoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,27 +21,32 @@ void		change_pwd(char *tab, int home)
 	i = 0;
 	if (home == 0)
 	{
-		while (tab[i] != '\0')
-			i++;
-		if (tab[i - 1] == '/')
-			tab[i - 1] = '\0';
-		ft_putendl("GNA");
-		tmp = ft_strjoin(ft_strjoin(find_value_envir("PWD"), "/"), tab);
+		i= ft_strlen(tab);
+		tab[i - 1] = ((tab[i - 1] == '/') ? '\0' : tab[i - 1]);
+		tmp = find_value_envir("PWD");
+		if (!ft_strncmp("..", tab, 2))
+		{
+			i = ft_strlen(tmp);
+			while (tmp[i] != '/')
+				i--;
+			tmp[i] = '\0';
+			i = 1;
+		}
+		tmp = ((i == 1) ? tmp : ft_strjoin(ft_strjoin(tmp, "/"), tab));
 		tmp = ft_strjoin("setenv ", ft_strjoin("PWD ", tmp));
-		ft_setenv(ft_strsplit(tmp, ' '));
 	}
 	else
 	{
 		tmp = ft_strjoin("setenv ", "PWD ");
 		tmp = ft_strjoin(tmp, find_value_envir("HOME"));
-		ft_setenv(ft_strsplit(tmp, ' '));
 	}
+	ft_setenv(ft_strsplit(tmp, ' '));
 	free(tmp);
 }
 
 int			ft_cd(char **tab)
 {
-	if (!ft_strncmp("~", tab[1], 1) || tab[1] == NULL || !ft_strcmp("$HOME", tab[1]))
+	if (!ft_strncmp("~", tab[1], 1) || !tab[1] || !ft_strcmp("$HOME", tab[1]))
 	{
 		ft_putendl("HOME");
 		tab[1] = ft_strdup(find_value_envir("HOME"));
@@ -53,10 +58,10 @@ int			ft_cd(char **tab)
 		change_pwd(tab[1], 1);
 	}
 	else if (chdir(tab[1]) == -1)
-		{
-			ft_putendl("error");
-			return (-1);
-		}
+	{
+		ft_putendl("error");
+		return (-1);
+	}
 	else
 		change_pwd(tab[1], 0);
 	return (0);
