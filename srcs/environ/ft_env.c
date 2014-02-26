396c2e977cac87c6bcf1b2489c768c38fabc9ca6
ivory9334 <aardjoun@e1r1p2.42.fr>
2014-02-26 10:20:29 +0100

    test sans le join du env

--- srcs/environ/ft_env.c
+++ srcs/environ/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: lsolofri <lsolofri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/25 00:46:22 by lsolofri          #+#    #+#             */
-/*   Updated: 2014/02/25 14:50:13 by lsolofri         ###   ########.fr       */
+/*   Updated: 2014/02/26 10:09:56 by aardjoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,8 @@ void	new_env(char **env)
 	while (*env)
 	{
 		tmp = ft_strsplit(*env, '=');
-		if (tmp[1] && (ft_strcmp(tmp[0], "HOME") == 0 || ft_strcmp(tmp[0], "PWD") == 0)) 
-				tmp[1] = ft_strjoin("/Volumes/DATA", tmp[1]);
+//		if (tmp[1] && (ft_strcmp(tmp[0], "HOME") == 0 || ft_strcmp(tmp[0], "PWD") == 0)) 
+		//			tmp[1] = ft_strjoin("/Volumes/DATA", tmp[1]);
 		g_env = add_env(g_env, tmp[0], tmp[1]);
 		env++;
 	}
