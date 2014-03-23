750e4baf08fba90c6ace0e8f7553339cb938faa6
Ne02ptzero <l.solofrizzo@gmail.com>
2014-03-23 21:50:18 +0100

    Fix aliases

--- srcs/functions/ft_insert_tab.c
+++ srcs/functions/ft_insert_tab.c
@@ -6,7 +6,7 @@
 /*   By: lsolofri <lsolofri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/05 14:18:59 by lsolofri          #+#    #+#             */
-/*   Updated: 2014/03/22 12:19:13 by rkharif          ###   ########.fr       */
+/*   Updated: 2014/03/23 21:46:22 by lsolofri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*join_tab_spe(char **tab, int start, int end)
 	char	*result;
 
 	result = NULL;
-	if (tab[start] && tab[end])
+	if (tab[start])
 	{
 		while (start < end)
 		{
@@ -57,7 +57,9 @@ char	**ft_insert_tab(char **src, char **alias, int i)
 	{
 		result = join_tab2(alias);
 		if (src[1])
+		{
 			result = ft_strjoin(result, join_tab_spe(src, 1, ft_tablen(src)));
+		}
 	}
 	else if (!src[i + 1])
 	{
